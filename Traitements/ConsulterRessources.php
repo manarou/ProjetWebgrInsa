9b34dfae2861f660a4bdd58bba283bf6f717791c
ibrahima <ibrahima@truc.fr>
2013-02-06 17:07:38 +0100

    Revert "fusion ibrahima"
    This reverts commit a475ab17a69f97d307a869da8c8d112332e32d46.

--- Traitements/ConsulterRessources.php
+++ Traitements/ConsulterRessources.php
@@ -11,55 +11,9 @@
 	include('../accueil.php'); 
 ?>
 <div>
-
-<?
-include('../database/connexion.php');
-
-
-				// On s'connecte à la BD
-				$bdd = connexionBD() or die ("<br> Echec de connexion à la base de données </br>");
-				
-				// Execution !
-				$requete = $bdd->prepare('SELECT Designation,Description,CoutHoraire FROM ressources');
-				$requete->execute());
-
-//$sql_ress="select * from ressources";
-//$query_ress=mysql_query($sql_ress) or die(mysql_error());
-?>
-<!-- <form action="ajout_participant.php" name="form1" method="post" onsubmit="return verif();"> -->
-<table align="center" width="62%" border="1">
-<caption>
-Liste des ressources
-</caption>
-
-<tr>
-   <th>Designation</th><th>description</th><th>Cout horaire</th>
-</tr>
-
-
-<?php
-/*
-//tant qu'il y a des ressoruces
-while ($donnees = $requete->fetch())
-				{
-					echo '<p> Ressource : ' . $donnees['Designation'] . '<br/></p>';
-				
-/* echo"<tr>
-      <td>$ress->Designation</td>
-      <td>$ress->Descrption</td>
-      <td>$ress->CoutHoraire</td>
-	  
-
-     </tr>";
-	
-}
-*/
-
-?>
-
-</table>
-<!-- </form> -->
-	
+	<p>
+		ici sont les ressources !
+	</p>
 </div>
 </body>
 </html>
